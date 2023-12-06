1ceaa7f66bae2d1bc196214eca7ab60db474a0db
Kenny Z <your.email@example.com>
2023-12-06 19:04:02 +0000

    feat: add usefetch + usefetch.test

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,34 +1,36 @@
 import { useState, useEffect } from "react";
 
-const useFetch = <T>(uri: string | null) => {
+const useFetch = <T>(uri: string | undefined) => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<T | null>(null);
   const [hasData, setHasData] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<unknown>(null);
   const [hasError, setHasError] = useState(false);
 
-  useEffect(() => {
+  const fetchData = async () => {
     if (!uri) {
       return;
     }
-    setLoading(true);
-    fetch(uri)
-      .then((response) => {
-        setLoading(false);
-        if (!response.ok) {
-          setHasError(true);
-          return { error: response.text };
-        }
-        return response.json();
-      })
-      .then((json) => {
-        setHasData(true);
-        setData(json);
-      })
-      .catch((err) => {
+    try {
+      setLoading(true);
+      const response = await fetch(uri);
+      if (!response.ok) {
         setHasError(true);
-        setError(err);
-      });
+        return;
+      }
+      const data = await response.json();
+      setData(data);
+      setHasData(true);
+    } catch (err: unknown) {
+      setHasError(true);
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
   }, [uri]);
 
   return {
@@ -41,3 +43,48 @@ const useFetch = <T>(uri: string | null) => {
 };
 
 export { useFetch };
+
+// import { useState, useEffect } from "react";
+
+// const useFetch = <T>(uri: string | null) => {
+//   const [loading, setLoading] = useState(false);
+//   const [data, setData] = useState<T | null>(null);
+//   const [hasData, setHasData] = useState(false);
+//   // const [error, setError] = useState(null);
+//   const [error, setError] = useState<unknown>(null);
+//   const [hasError, setHasError] = useState(false);
+
+//   useEffect(() => {
+//     if (!uri) {
+//       return;
+//     }
+//     setLoading(true);
+//     fetch(uri)
+//       .then((response) => {
+//         setLoading(false);
+//         if (!response.ok) {
+//           setHasError(true);
+//           return { error: response.text };
+//         }
+//         return response.json();
+//       })
+//       .then((json) => {
+//         setHasData(true);
+//         setData(json);
+//       })
+//       .catch((err) => {
+//         setHasError(true);
+//         setError(err);
+//       });
+//   }, [uri]);
+
+//   return {
+//     loading,
+//     data,
+//     hasData,
+//     error,
+//     hasError,
+//   };
+// };
+
+// export { useFetch };
