796cada172f0279b591c7219ba9a8d54ca34c609
Kenny Z <your.email@example.com>
2023-12-08 00:04:48 +0000

    fix: fixed uri to null instead of undefined in use-fetch.ts

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useFetch = <T>(uri: string | undefined) => {
+const useFetch = <T>(uri: string | null) => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<T | null>(null);
   const [hasData, setHasData] = useState(false);
@@ -43,48 +43,3 @@ const useFetch = <T>(uri: string | undefined) => {
 };
 
 export { useFetch };
-
-// import { useState, useEffect } from "react";
-
-// const useFetch = <T>(uri: string | null) => {
-//   const [loading, setLoading] = useState(false);
-//   const [data, setData] = useState<T | null>(null);
-//   const [hasData, setHasData] = useState(false);
-//   // const [error, setError] = useState(null);
-//   const [error, setError] = useState<unknown>(null);
-//   const [hasError, setHasError] = useState(false);
-
-//   useEffect(() => {
-//     if (!uri) {
-//       return;
-//     }
-//     setLoading(true);
-//     fetch(uri)
-//       .then((response) => {
-//         setLoading(false);
-//         if (!response.ok) {
-//           setHasError(true);
-//           return { error: response.text };
-//         }
-//         return response.json();
-//       })
-//       .then((json) => {
-//         setHasData(true);
-//         setData(json);
-//       })
-//       .catch((err) => {
-//         setHasError(true);
-//         setError(err);
-//       });
-//   }, [uri]);
-
-//   return {
-//     loading,
-//     data,
-//     hasData,
-//     error,
-//     hasError,
-//   };
-// };
-
-// export { useFetch };
